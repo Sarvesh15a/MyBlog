78adebc59ee5cdef59c47848c28f0895eaf8e9c3
Sarvesh15a <yadsarvesh15@gmail.com>
2024-03-14 20:10:28 +0530

    changes

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -129,10 +129,10 @@ export default function UpdatePost() {
             }
             value={formData.category}
           >
-            <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+                  <option value='uncategorized' > Select a category</option>
+                  <option value='social' >Socical</option>
+                  <option value='education' >Education</option>
+                  <option value='entertainment' >Entertainment</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
